b9245e88f46507912d67f6cc00a0c8a509750176
Sebastian Ochoa <sebastian.ochoa@grupobusiness.it>
2025-09-02 09:08:41 -0500

    [ADD]: Project scaffolding and  custom code component

---
+++ CustomCodeComponent/apiService.ts
@@ -0,0 +1,12 @@
+﻿export async function fetchApiData<T = unknown>(url = 'https://jsonplaceholder.typicode.com/posts/1'): Promise<T> {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error en la respuesta del API: ${response.status}`);
+        }
+        return (await response.json()) as T;
+    } catch (error) {
+        console.error('Error fetching API:', error);
+        throw error;
+    }
+}
